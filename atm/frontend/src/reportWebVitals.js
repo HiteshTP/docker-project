5d457c2945d5157487c35fa73d6b833d12a1ecca
HiteshTP <155635465+HiteshTP@users.noreply.github.com>
2024-11-14 23:08:33 +0530

    Create reportWebVitals.js

---
+++ atm/frontend/src/reportWebVitals.js
@@ -0,0 +1,6 @@
+// /frontend/src/reportWebVitals.js
+const reportWebVitals = (metric) => {
+  console.log(metric);  // You can send these metrics to an analytics service
+};
+
+export default reportWebVitals;
