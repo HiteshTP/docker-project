59b7bb2b0a37e681c08839bacef5c8cb3205f4ad
HiteshTP <155635465+HiteshTP@users.noreply.github.com>
2024-11-19 11:40:55 +0530

    Update App.js

--- atm/frontend/src/App.js
+++ atm/frontend/src/App.js
@@ -1,24 +1,22 @@
-import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import LoginPage from "./pages/LoginPage";
-import Dashboard from "./pages/Dashboard";
-import TransactionPage from "./pages/TransactionPage";
-import Navbar from "./components/Navbar";
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import LoginPage from './pages/LoginPage';
+import Dashboard from './pages/Dashboard';
+import TransactionPage from './pages/TransactionPage';
+import Navbar from './components/Navbar';
 import './App.css';
 
-function App() {
+const App = () => {
   return (
     <Router>
       <Navbar />
-      <div className="container">
-        <Routes>
-          <Route path="/" element={<LoginPage />} />
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/transactions" element={<TransactionPage />} />
-        </Routes>
-      </div>
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/transactions" element={<TransactionPage />} />
+      </Routes>
     </Router>
   );
-}
+};
 
 export default App;
