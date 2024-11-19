e21c11a81caaf7879204c35a0101bd1d2057b26b
HiteshTP <155635465+HiteshTP@users.noreply.github.com>
2024-11-19 12:11:21 +0530

    Update server.js

--- atm/user-service/src/server.js
+++ atm/user-service/src/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
-const userRoutes = require('./routes/userRoutes');
+const userRoutes = require('./routes/userRouters');
 
 dotenv.config();
 const app = express();
