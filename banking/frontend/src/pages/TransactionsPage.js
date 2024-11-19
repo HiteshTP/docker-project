c42b62b6247fd68a8f793c7d2ce50b1cc11de2e7
HiteshTP <155635465+HiteshTP@users.noreply.github.com>
2024-11-19 17:01:56 +0530

    Update TransactionsPage.js

--- banking/frontend/src/pages/TransactionsPage.js
+++ banking/frontend/src/pages/TransactionsPage.js
@@ -1,23 +1,7 @@
-import React from "react";
+import React from 'react';
 
 function TransactionsPage() {
-  const transactions = [
-    { id: 1, date: "2024-11-01", amount: 100, type: "Credit" },
-    { id: 2, date: "2024-11-02", amount: 50, type: "Debit" },
-  ];
-
-  return (
-    <div>
-      <h2>Transaction History</h2>
-      <ul>
-        {transactions.map((txn) => (
-          <li key={txn.id}>
-            {txn.date}: {txn.type} of ${txn.amount}
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
+  return <h1>Transactions Page</h1>;
 }
 
 export default TransactionsPage;
