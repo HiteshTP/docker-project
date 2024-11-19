860e3983882dba3289c5b02f159807033852f547
HiteshTP <155635465+HiteshTP@users.noreply.github.com>
2024-11-19 11:47:57 +0530

    Update userModel.js

--- atm/user-service/src/models/userModel.js
+++ atm/user-service/src/models/userModel.js
@@ -1,12 +1,9 @@
 const mongoose = require('mongoose');
 
-// Define schema for the user model
-const userSchema = new mongoose.Schema({
-  username: { type: String, required: true },
+const userSchema = mongoose.Schema({
+  name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
 });
 
-const User = mongoose.model('User', userSchema);
-
-module.exports = User;
+module.exports = mongoose.model('User', userSchema);
