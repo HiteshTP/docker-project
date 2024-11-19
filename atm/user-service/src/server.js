263a034261c7905cde2e8668fac51e4096f8f10c
HiteshTP <155635465+HiteshTP@users.noreply.github.com>
2024-11-19 13:13:57 +0530

    Update server.js

--- atm/user-service/src/server.js
+++ atm/user-service/src/server.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
-const userRoutes = require('./routes/userRouters');
+const userRoutes = require('./routes/userRoutes');
 
 dotenv.config();
 const app = express();
 
 app.use(express.json());
+app.get('/', (req, res) => {
+  res.send('User Service is running!');
+});
+
 app.use('/api/users', userRoutes);
 
 const PORT = process.env.PORT || 5000;
