d06c0102512f6ac565b8acac7e4207efc4d31803
HiteshTP <155635465+HiteshTP@users.noreply.github.com>
2024-11-19 11:42:22 +0530

    Delete atm/frontend/src/reportWebVitals.js

--- atm/frontend/src/reportWebVitals.js
+++
@@ -1,6 +0,0 @@
-// /frontend/src/reportWebVitals.js
-const reportWebVitals = (metric) => {
-  console.log(metric);  // You can send these metrics to an analytics service
-};
-
-export default reportWebVitals;
