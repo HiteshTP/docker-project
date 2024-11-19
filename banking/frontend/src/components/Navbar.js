194494e5d6c763199ec442c96e74f9ddef61cc54
HiteshTP <155635465+HiteshTP@users.noreply.github.com>
2024-11-19 16:59:14 +0530

    Update Navbar.js

--- banking/frontend/src/components/Navbar.js
+++ banking/frontend/src/components/Navbar.js
@@ -1,17 +1,13 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import "./Navbar.css";
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Navbar() {
   return (
-    <nav className="navbar">
-      <h1>Banking App</h1>
-      <ul>
-        <li><Link to="/">Login</Link></li>
-        <li><Link to="/account">Account</Link></li>
-        <li><Link to="/transactions">Transactions</Link></li>
-        <li><Link to="/admin">Admin</Link></li>
-      </ul>
+    <nav>
+      <Link className="nav-link" to="/">Home</Link>
+      <Link className="nav-link" to="/account">Account</Link>
+      <Link className="nav-link" to="/transactions">Transactions</Link>
+      <Link className="nav-link" to="/admin">Admin</Link>
     </nav>
   );
 }
