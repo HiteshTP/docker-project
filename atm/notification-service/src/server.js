9c33eb37a67ae9f55dbb9464101f5e53a2c8f02c
HiteshTP <155635465+HiteshTP@users.noreply.github.com>
2024-11-20 11:31:33 +0530

    Update server.js

--- atm/notification-service/src/server.js
+++ atm/notification-service/src/server.js
@@ -1,1 +1,16 @@
+const express = require('express');
+const app = express();
+const notificationRoutes = require('./routes/notificationRoutes');
+
+// Middleware
+app.use(express.json());
+
+// Routes
+app.use('/api/notifications', notificationRoutes);
+
+// Server setup
+const PORT = 4002;
+app.listen(PORT, () => {
+  console.log(`Notification Service running on port ${PORT}`);
+});
 
