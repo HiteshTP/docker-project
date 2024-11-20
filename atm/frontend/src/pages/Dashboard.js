848af168467ea65bd90f886460fd81a02dff294d
HiteshTP <155635465+HiteshTP@users.noreply.github.com>
2024-11-20 10:53:37 +0530

    Dashboard.js

--- atm/frontend/src/pages/Dashboard.js
+++ atm/frontend/src/pages/Dashboard.js
@@ -1,12 +1,1 @@
-import React from 'react';
 
-const Dashboard = () => {
-  return (
-    <div>
-      <h1>Dashboard</h1>
-      <p>Welcome to your account!</p>
-    </div>
-  );
-};
-
-export default Dashboard;
