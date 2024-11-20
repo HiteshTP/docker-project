d3f57311abb092fb99174f5c67aedd2555eb8236
HiteshTP <155635465+HiteshTP@users.noreply.github.com>
2024-11-20 15:40:15 +0530

    Update server.js

--- voting-app/backend/src/server.js
+++ voting-app/backend/src/server.js
@@ -1,1 +1,18 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const voteRoutes = require("./routes/voteRoutes");
+const resultRoutes = require("./routes/resultRoutes");
+
+const app = express();
+app.use(express.json());
+
+mongoose
+  .connect("mongodb://database:27017/voting-app", { useNewUrlParser: true })
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.error(err));
+
+app.use("/api/votes", voteRoutes);
+app.use("/api/results", resultRoutes);
+
+app.listen(5000, () => console.log("Server running on port 5000"));
 
