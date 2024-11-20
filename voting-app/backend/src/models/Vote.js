cd27b5414332e91e09540df3dab3f19bfea0eac6
HiteshTP <155635465+HiteshTP@users.noreply.github.com>
2024-11-20 15:28:28 +0530

    Add files via upload

---
+++ voting-app/backend/src/models/Vote.js
@@ -0,0 +1,7 @@
+const mongoose = require('mongoose');
+
+const voteSchema = new mongoose.Schema({
+    candidate: { type: String, required: true },
+});
+
+module.exports = mongoose.model('Vote', voteSchema);
