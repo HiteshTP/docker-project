958b2cb2698a1c1c47571daa8214c290373b8f64
HiteshTP <155635465+HiteshTP@users.noreply.github.com>
2024-11-20 11:28:50 +0530

    Update appointmentRoutes.js

--- atm/appointment-service/src/routes/appointmentRoutes.js
+++ atm/appointment-service/src/routes/appointmentRoutes.js
@@ -1,1 +1,17 @@
+const express = require('express');
+const {
+  createAppointment,
+  getAppointments,
+  getAppointmentById,
+  deleteAppointment
+} = require('../controllers/appointmentController');
+
+const router = express.Router();
+
+router.post('/', createAppointment);
+router.get('/', getAppointments);
+router.get('/:id', getAppointmentById);
+router.delete('/:id', deleteAppointment);
+
+module.exports = router;
 
