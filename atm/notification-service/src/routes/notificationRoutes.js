c8f4e04fc0a042bb51fed0ce29f9a3b364167a67
HiteshTP <155635465+HiteshTP@users.noreply.github.com>
2024-11-20 11:31:14 +0530

    Update notificationRoutes.js

--- atm/notification-service/src/routes/notificationRoutes.js
+++ atm/notification-service/src/routes/notificationRoutes.js
@@ -1,1 +1,9 @@
+const express = require('express');
+const router = express.Router();
+const { sendNotification } = require('../controllers/notificationController');
+
+// Define route for sending notifications
+router.post('/send', sendNotification);
+
+module.exports = router;
 
