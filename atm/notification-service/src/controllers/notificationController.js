ad5bf2067b88ba8e7e784bd831d7a8c91e2b67f6
HiteshTP <155635465+HiteshTP@users.noreply.github.com>
2024-11-20 11:14:13 +0530

    Update notificationController.js

--- atm/notification-service/src/controllers/notificationController.js
+++ atm/notification-service/src/controllers/notificationController.js
@@ -1,12 +1,1 @@
-const sendNotification = (req, res) => {
-    const { userId, message } = req.body;
 
-    // Simulate sending a notification
-    console.log(`Notification sent to user ${userId}: ${message}`);
-
-    res.status(200).json({ success: true, message: 'Notification sent successfully' });
-};
-
-module.exports = {
-    sendNotification,
-};
