5f443bcf60f9351a2c604d1830d5afc6dd0c212e
HiteshTP <155635465+HiteshTP@users.noreply.github.com>
2024-11-20 11:23:32 +0530

    Update AppointmentList.js

--- atm/frontend/src/components/AppointmentList.js
+++ atm/frontend/src/components/AppointmentList.js
@@ -1,1 +1,12 @@
+import React from 'react';
+
+const AppointmentList = ({ appointments }) => (
+  <ul>
+    {appointments.map((appointment, index) => (
+      <li key={index}>{appointment.date} - {appointment.description}</li>
+    ))}
+  </ul>
+);
+
+export default AppointmentList;
 
