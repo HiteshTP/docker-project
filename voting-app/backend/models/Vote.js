4be9aee474e41653c2fb0c0d502a0582e434b415
HiteshTP <155635465+HiteshTP@users.noreply.github.com>
2024-11-20 15:24:15 +0530

    Delete voting-app/backend/models directory

--- voting-app/backend/models/Vote.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose');
-
-const voteSchema = new mongoose.Schema({
-    candidate: { type: String, required: true },
-});
-
-module.exports = mongoose.model('Vote', voteSchema);
