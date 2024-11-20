1653f2be6613b1975f7dabc6aa3ad9570de2157c
HiteshTP <155635465+HiteshTP@users.noreply.github.com>
2024-11-20 15:41:08 +0530

    Update voteRoutes.js

--- voting-app/backend/src/routes/voteRoutes.js
+++ voting-app/backend/src/routes/voteRoutes.js
@@ -1,1 +1,8 @@
+const express = require("express");
+const { castVote } = require("../controllers/voteController");
+const router = express.Router();
+
+router.post("/", castVote);
+
+module.exports = router;
 
