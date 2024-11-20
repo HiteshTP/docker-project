0ebe0828ad3d91d3cc5388125c6fabc24140aaf6
HiteshTP <155635465+HiteshTP@users.noreply.github.com>
2024-11-20 11:14:51 +0530

    Update server.js

--- atm/notification-service/src/server.js
+++ atm/notification-service/src/server.js
@@ -1,18 +1,1 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const notificationRoutes = require('./routes/notificationRoutes');
-require('dotenv').config();
 
-const app = express();
-const PORT = process.env.PORT || 3000;
-
-// Middleware
-app.use(bodyParser.json());
-
-// Routes
-app.use('/api/notifications', notificationRoutes);
-
-// Start server
-app.listen(PORT, () => {
-    console.log(`Notification Service running on port ${PORT}`);
-});
