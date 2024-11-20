fe73d24a6ec32778dc2bd5f171fccd538c7d1912
HiteshTP <155635465+HiteshTP@users.noreply.github.com>
2024-11-20 11:28:13 +0530

    Update appointmentModel.js

--- atm/appointment-service/src/models/appointmentModel.js
+++ atm/appointment-service/src/models/appointmentModel.js
@@ -1,1 +1,11 @@
+const mongoose = require('mongoose');
+
+const appointmentSchema = new mongoose.Schema({
+  patientId: { type: mongoose.Schema.Types.ObjectId, ref: 'Patient', required: true },
+  doctor: { type: String, required: true },
+  date: { type: Date, required: true },
+  reason: { type: String, required: true }
+});
+
+module.exports = mongoose.model('Appointment', appointmentSchema);
 
