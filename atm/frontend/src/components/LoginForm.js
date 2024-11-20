2b3ab5506a6dc5e70a364f1f4bad20f72e977e0c
HiteshTP <155635465+HiteshTP@users.noreply.github.com>
2024-11-20 11:22:53 +0530

    Update LoginForm.js

--- atm/frontend/src/components/LoginForm.js
+++ atm/frontend/src/components/LoginForm.js
@@ -1,1 +1,30 @@
 
+import React, { useState } from 'react';
+
+const LoginForm = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log({ username, password });
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        Username:
+        <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} />
+      </label>
+      <br />
+      <label>
+        Password:
+        <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+      </label>
+      <br />
+      <button type="submit">Login</button>
+    </form>
+  );
+};
+
+export default LoginForm;
